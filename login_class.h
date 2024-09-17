214c4061815e45bc645f5299e86382348f9435ea
Laurence <plawren1019@gmail.com>
2024-09-17 13:57:33 +0800

    Update login_class.h

--- login_class.h
+++ login_class.h
@@ -5,27 +5,27 @@
 
 class admin_class
 {
-public:
-    string username = "";
-    string userpass = "";
-    int ad_choice = 0;
-    const string realname = "admin";
-    const string realpass = "pass";
+    public:
+        string username = "";
+        string userpass = "";
+        int ad_choice = 0;
+        const string realname = "admin";
+        const string realpass = "pass";
 };
 
 class cust_class
 {
    public:
-      const string cust_memID[3][2] = {
-               {"012345678901","Laurence"},
-               {"456789098768","Daryl"},
-               {"987654321098","Cindy"}
-      };
-      bool cust_mem_log = 0; //For After Member Discount Usage
+        const string cust_memID[3][2] = {
+                {"012345678901","Laurence"},
+                {"456789098768","Daryl"},
+                {"987654321098","Cindy"}
+        };
+        bool cust_mem_log = 0; //For After Member Discount Usage
 
-      string record[1][5] = {
+        string record[1][5] = {
                {"Movie","Date","Time","Category","Seat"}
-      };
+        };
 };
 
 #endif
