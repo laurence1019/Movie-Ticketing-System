9d9054619dc066ff349d014f8dc19818d06c2381
Laurence <plawren1019@gmail.com>
2024-09-17 14:02:41 +0800

    Update movie_class.h

--- movie_class.h
+++ movie_class.h
@@ -5,7 +5,8 @@
 #include "ticket_category.h"
 
 // MOVIE CLASS DEFINITION
-class Movie {
+class Movie 
+{
 public:
     // declare variables 
     array<string, 10> alphabs = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
@@ -35,9 +36,11 @@ public:
     // }
     map<string, map<string, vector<pair<string, string>>>> initialize_booking(vector<string> dates, vector<string> times) {
         map<string, map<string, vector<pair<string, string>>>> main_dict;
-        for (string date : dates) {
+        for (string date : dates) 
+        {
             map<string, vector<pair<string, string>>> plc_dict;
-            for (string time : times) {
+            for (string time : times) 
+            {
                 plc_dict[time] = vector<pair<string, string>>();  // Initialize empty seat vector for each time
             }
             main_dict[date] = plc_dict;
@@ -46,7 +49,8 @@ public:
     }
 
     // Print movie information
-    void print_info() {
+    void print_info() 
+    {
         cout << "Movie name: " << name << endl;
         cout << "Movie length: " << length << endl;
         cout << "Movie genre: " << genre << endl;
@@ -54,21 +58,25 @@ public:
     }
 
     // Input for date selection with validation
-    string in_date() {
+    string in_date() 
+    {
         cout << "This is the available dates: ";
         // for date in dates, looping throught one at a time. Think of indexing
-        for (string date : dates) {
+        for (string date : dates) 
+        {
             cout << date << ", ";
         }
         cout << endl;
 
         string chosen_date;
-        while (true) {
+        while (true) 
+        {
             cout << "Enter a valid date: ";
             cin >> chosen_date;
             // find(start of range, end of range, variable to be found)
             // If found return variable, else return end of range
-            if (find(dates.begin(), dates.end(), chosen_date) != dates.end()) {
+            if (find(dates.begin(), dates.end(), chosen_date) != dates.end()) 
+            {
                 return chosen_date;
             }
             cout << "Invalid date, please try again." << endl;
@@ -76,21 +84,25 @@ public:
     }
 
     // Input for time selection with validation
-    string in_time() {
+    string in_time() 
+    {
         cout << "This is the available times: ";
         // for time in times, looping throught one at a time. Think of indexing
-        for (string t : time) {
+        for (string t : time) 
+        {
             cout << t << ", ";
         }
         cout << endl;
 
         string chosen_time;
-        while (true) {
+        while (true) 
+        {
             cout << "Enter a valid time: ";
             cin >> chosen_time;
             // find(start of range, end of range, variable to be found)
             // If found return variable, else return end of range
-            if (find(time.begin(), time.end(), chosen_time) != time.end()) {
+            if (find(time.begin(), time.end(), chosen_time) != time.end()) 
+            {
                 return chosen_time;
             }
             cout << "Invalid time, please try again." << endl;
@@ -98,7 +110,8 @@ public:
     }
 
     // Return booked seats for a given date and time
-    vector<pair<string, string>>& return_seats(string date, string time) {
+    vector<pair<string, string>>& return_seats(string date, string time) 
+    {
         // dictionary is called with keys
         // Key : Value
         // Nested Dictionary is called with two or more keys
@@ -106,19 +119,24 @@ public:
     }
 
     // Show available and booked seats
-    void show_seats(const vector<pair<string, string>>& booked_seats) {
+    void show_seats(const vector<pair<string, string>>& booked_seats) 
+    {
         cout << "   1   2   3   4   5   6   7   8   9   10" << endl; // hard coded, can use setw
 
-        for (string letter : alphabs) {
+        for (string letter : alphabs) 
+        {
             cout << letter << " ";
-            for (string col : list_nums) {
+            for (string col : list_nums) 
+            {
                 // find(start of range, end of range, variable to be found)
                 // If found return variable, else return end of range
                 // make_pair(letter, col) makes a pair which is what our booked_seats var type is. It just combines it into a tuple / pair
-                if (find(booked_seats.begin(), booked_seats.end(), make_pair(letter, col)) != booked_seats.end()) {
+                if (find(booked_seats.begin(), booked_seats.end(), make_pair(letter, col)) != booked_seats.end()) 
+                {
                     cout << "|XX|"; // occupied
                 }
-                else {
+                else 
+                {
                     cout << "|  |"; // unoccupied
                 }
             }
@@ -127,16 +145,19 @@ public:
     }
 
     // Input for seat selection with validation
-    pair<string, string> in_seats(string date, string time) {
+    pair<string, string> in_seats(string date, string time) 
+    {
         string row_alpha, col_num;
-        while (true) {
+        while (true) 
+        {
             cout << "Enter seat letter (A-J): ";
             cin >> row_alpha;
             transform(row_alpha.begin(), row_alpha.end(), row_alpha.begin(), ::toupper); // transform string to upper. Could use char, but I'm more comfortable with strings
             // find(start of range, end of range, variable to be found)
             // If found return variable, else return end of range
             // Our parameters are our gobals declared at the header
-            if (find(alphabs.begin(), alphabs.end(), row_alpha) == alphabs.end()) {
+            if (find(alphabs.begin(), alphabs.end(), row_alpha) == alphabs.end()) 
+            {
                 cout << "Invalid row. Please try again." << endl;
                 continue;
             }
@@ -150,7 +171,8 @@ public:
             // find(start of range, end of range, variable to be found)
             // If found return variable, else return end of range
             // Our parameters are our gobals declared at the header
-            if (find(list_nums.begin(), list_nums.end(), col_num) == list_nums.end()) {
+            if (find(list_nums.begin(), list_nums.end(), col_num) == list_nums.end()) 
+            {
                 cout << "Invalid seat number. Please try again." << endl;
                 continue;
             }
@@ -158,11 +180,13 @@ public:
 
             pair<string, string> seat_n = make_pair(row_alpha, col_num);
             // return_seats returns our seat vector
-            if (find(return_seats(date, time).begin(), return_seats(date, time).end(), seat_n) != return_seats(date, time).end()) {
+            if (find(return_seats(date, time).begin(), return_seats(date, time).end(), seat_n) != return_seats(date, time).end()) 
+            {
                 // checks for booked seat using find function
                 cout << "Seat already booked. Please choose another seat." << endl;
             }
-            else {
+            else 
+            {
                 return_seats(date, time).push_back(seat_n); // append seat_n into seat vector
                 cout << "\nSeat booked successfully!\n";
                 show_seats(return_seats(date, time));
@@ -172,17 +196,20 @@ public:
     }
 
     // Main booking function
-    tuple<string, string, pair<string, string>, string> booking(string date, string time, int ticket_amt) {
+    tuple<string, string, pair<string, string>, string> booking(string date, string time, int ticket_amt) 
+    {
         // we check if date and time are empty, this is for booking multiple tickets
         // if false we enter date and time, if true we skip 
         // So users don't have to enter date and time multiple times
-        if (date.empty() && time.empty()) {
+        if (date.empty() && time.empty()) 
+        {
             date = in_date();
             time = in_time();
         }
 
         // check if the hall is full by checking len of seat vector, hard coded limit 100
-        if ((return_seats(date, time).size() + ticket_amt) >= 100) {
+        if ((return_seats(date, time).size() + ticket_amt) >= 100) 
+        {
             cout << "Sorry, all seats are booked. Please try another date and time." << endl;
             return make_tuple("", "", make_pair("", ""), "");
         }
